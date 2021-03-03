0988866a0cbea65ad0cb6b6c72b958755c642041
NowApoca <diez.manuel98@gmail.com>
2021-03-02 21:00:49 -0300

    freeze object

--- katas/oop_age_of_empires/20210302/factories/Stats.js
+++ katas/oop_age_of_empires/20210302/factories/Stats.js
@@ -1,26 +1,81 @@
+'use strict';
 
 const model = () => {
 
-    return {
-        armor: null,
-        hp: null,
-        current_hp: null,
-        dp: null,
-        scope: null,
-        init({hp, armor, dp, scope = 1}){
-            this.armor = armor || 0
-            this.hp = hp || 1
-            this.current_hp = hp || 1
-            this.dp = dp || 0
-            this.scope = scope || 1
-        },
+    let is_dead = false
+
+
+    let armor = null;
+    let hp = null;
+    let current_hp = null;
+    let dp = null;
+    let scope = null;
+    let penetration = null;
+
+    const get_armor = () => armor;
+    const get_hp = () => hp;
+    const get_current_hp = () => current_hp;
+    const get_dp = () => dp;
+    const get_scope = () => scope;
+    const get_penetration = () => penetration;
+
+    const update_is_dead_attribute = () => {
+        if(current_hp <= 0){
+            current_hp = 0;
+            is_dead = true
+        }else{
+            is_dead = false
+        }
     }
+
+    const update_current_hp_attribute = () => {
+        if(current_hp > hp){
+            current_hp = hp
+        }
+    }
+
+    // PERHAPS IT IS BETTER HAVING GETTERS AND SETTERS. VARS CAN BE CHANGED IN THE ENTIRE PROGRAM. PERHAPS, WE SHOULD RETURN A FREEZE.
+
+    let obj = {
+        get_armor,
+        get_hp,
+        get_current_hp,
+        get_dp,
+        get_scope,
+        get_penetration,
+
+        init(stats){
+            armor = stats.armor || 0
+            hp = stats.hp || 1
+            current_hp = stats.hp || 1
+            dp = stats.dp || 0
+            scope = stats.scope || 1
+            penetration = stats.penetration || 0
+            update_is_dead_attribute(this)
+        },
+
+        receive_atack(atacker_stats){
+            current_hp -= atacker_stats.get_dp() + atacker_stats.get_penetration() - armor
+            update_is_dead_attribute(this)
+            return this
+        },
+        is_dead(){
+            return is_dead
+        },
+        heal(hp){
+            current_hp += hp
+            update_current_hp_attribute(this)
+            return this
+        }
+    }
+    Object.freeze(obj)
+    return obj 
 }
 
 const Stats = () => {
 
     const create = () => {
-        return Object.assign({}, model())
+        return model()
     }
 
     return {
--- katas/oop_age_of_empires/20210302/test/unit-test/stats.test.js
+++ katas/oop_age_of_empires/20210302/test/unit-test/stats.test.js
@@ -1,21 +1,68 @@
+'use strict';
+
 const expect = require('expect')
 const MeleeUnit = require('../../factories/MeleeUnit')
 const Stats = require('../../factories/Stats')
 
-const statsTest = {hp: 15, armor: 2, dp: 3}
+const stats_test = {hp: 45, armor: 2, dp: 3}
 
 describe(' Unit creation ', () => {
     it('Create stats', () => {
         const stats = Stats.create()
-        stats.init(statsTest)
-        expect(stats.hp).toEqual(statsTest.hp)
-        expect(stats.armor).toEqual(statsTest.armor)
-        expect(stats.dp).toEqual(statsTest.dp)
-        expect(stats.scope).toEqual(1)
+        stats.init(stats_test)
+        expect(stats.get_hp()).toEqual(stats_test.hp)
+        expect(stats.get_armor()).toEqual(stats_test.armor)
+        expect(stats.get_dp()).toEqual(stats_test.dp)
+        expect(stats.get_scope()).toEqual(1)
+        expect(stats.get_penetration()).toEqual(0)
     })
     it('Recive an atack to the unit stats', () => {
         const stats = Stats.create()
-        stats.init(statsTest)
-        stats.receive_an_atack({dp: 5, penetration: 2})
+        stats.init(stats_test)
+        const atacker_stats = {
+            dp: 10,
+            penetration: 2,
+        }
+        const stats_atacker = Stats.create()
+        stats_atacker.init(atacker_stats)
+        let stats_after_defend = stats.receive_atack(stats_atacker)
+        expect(stats_after_defend.get_current_hp()).toEqual(stats.get_hp() + stats.get_armor() - stats_atacker.get_dp() - stats_atacker.get_penetration())
+    })
+    it('Recive an atack to the unit and kill the defended stats', () => {
+        const stats = Stats.create()
+        stats.init(stats_test)
+        const atacker_stats = {
+            dp: 100,
+            penetration: 2,
+        }
+        const stats_atacker = Stats.create()
+        stats_atacker.init(atacker_stats)
+        let stats_after_defend = stats.receive_atack(stats_atacker)
+        expect(stats_after_defend.get_current_hp()).toEqual(0)
+        expect(stats_after_defend.is_dead()).toEqual(true)
+    })
+    it('Recive an atack to the unit and heal after atack', () => {
+        const stats = Stats.create()
+        stats.init(stats_test)
+        const atacker_stats = {
+            dp: 10,
+            penetration: 2,
+        }
+        const stats_atacker = Stats.create()
+        stats_atacker.init(atacker_stats)
+        stats.receive_atack(stats_atacker)
+        const healed_points = 10
+        let stats_after_heal = stats.heal(healed_points)
+        expect(stats_after_heal.get_current_hp()).toEqual(stats.get_hp() + stats.get_armor() - 
+            stats_atacker.get_dp() - stats_atacker.get_penetration() + healed_points)
+    })
+})
+
+
+describe(' Throw stats cases ', () => {
+    it('Create stats stats should not be extensiblle', () => {
+        const stats = Stats.create()
+        stats.init(stats_test)
+        expect(() => stats.armor = 5).toThrowError('Cannot add property armor, object is not extensible')
     })
 })
